3f172541b58d168776871c9c9f46706754612737
Khai Hong Goh <khaihong.goh@gmail.com>
2020-04-10 18:00:43 +0800

    Registration bug fixed
    Check if there is existing email address/username.

--- APIs/accountRoute.js
+++ APIs/accountRoute.js
@@ -5,6 +5,17 @@ const route = express.Router();
 
 route.post('/', async (req, res) => {
   const { username, password, firstName, lastName, email } = req.body;
+  const existingUserName = await Account.findOne({ username });
+  
+  if (existingUserName) {
+	
+	res.json({ 	
+		message: "The username already exists. Please use a different username",
+		success: false
+	});
+
+	}  
+  
   let account = {};
   account.username = username;
   account.password = password;
